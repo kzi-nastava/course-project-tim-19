9f8aab4046a654c1f57023edfc09f8df9680ca8c
popovbojana <96697478+popovbojana@users.noreply.github.com>
2022-04-30 21:21:21 +0200

    Added classes for modification requests.

---
+++ ModificationRequest/ModificationRequest.cs
@@ -0,0 +1,20 @@
+using Newtonsoft.Json;
+public class ModificationRequest{
+
+    public int appointmentsId { get; set; }
+    public int doctorsId { get; set; }
+    public DateTime dateTime { get; set; }
+    public Emergency emergency { get; set; }
+
+    public ModificationRequest(int id, int doctorsId, DateTime dateTime, Emergency emergency){
+        this.appointmentsId = id;
+        this.doctorsId = doctorsId;
+        this.dateTime = dateTime;
+        this.emergency = emergency;
+    }
+
+    public override string ToString()
+    {
+        return "ModificationRequest [appointmentsId: " + appointmentsId + "]";
+    }
+}
