fa486862634d996812043867676c561f043e2530
AndreaSV692020 <andrea.katzenberger01@gmail.com>
2022-06-13 23:20:55 +0200

    Added class for day off requests

---
+++ DayOffRequests/DayOffRequest.cs
@@ -0,0 +1,23 @@
+public class DayOffRequest
+{
+    public int doctorId{ get; set; }
+    public string date { get; set; }
+    public int duration { get; set; }
+    public string state { get; set; }
+    public string cause { get; set; }
+
+    public DayOffRequest(int doctorId, string date, int duration,string state,string cause){
+        this.doctorId = doctorId;
+        this.date = date;
+        this.duration = duration;
+        this.state = state;
+        this.cause = cause;
+    }
+
+    public override string ToString()
+    {
+        return "DayOffRequest [doctor id:" + doctorId+", date: "+date+", duration: "+duration+", state: "+state+", cause: "+cause +"]";
+        
+    }
+
+}
